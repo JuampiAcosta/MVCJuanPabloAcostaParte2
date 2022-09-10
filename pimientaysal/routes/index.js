926f8bb2160e0e52a20e35e5b3d85ab72c354ac7
JuampiAcosta <acostajuan678@gmail.com>
2022-09-10 00:26:20 -0300

    Primer Proyecto EJS

--- pimientaysal/routes/index.js
+++ pimientaysal/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const maincontroller = require("../controllers/maincontroller");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
+router.get('/', maincontroller.home);
+router.get("/detalleMenu/:id",maincontroller.detalleMenu)
 module.exports = router;
